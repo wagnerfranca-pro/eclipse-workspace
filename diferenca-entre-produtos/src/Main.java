f6bf23024fa3ddb0980e0e074347b3a75a9c4cdd
wagnerfranca-pro <wagnerdesouzafranca@gmail.com>
2026-02-24 10:16:27 -0300

    Criado diferenca-entre-produtos

---
+++ diferenca-entre-produtos/src/Main.java
@@ -0,0 +1,28 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int A, B, C, D, dif;
+		
+		A = sc.nextInt();
+		B = sc.nextInt();
+		C = sc.nextInt();
+		D = sc.nextInt();
+		
+		dif = (A*B-C*D);
+		
+		System.out.println(dif);
+		
+		sc.close();
+		
+
+	}
+
+}
