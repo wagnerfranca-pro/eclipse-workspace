5d46e2e9a3804dc47a51a6f0ffec87a342fb06dd
Wagner de Souza França <wagnerdesouzafranca@gmail.com>
2026-03-02 20:54:12 -0300

    Exercicio intervalos refeito e corrigido.

--- intervalos/src/Main.java
+++ intervalos/src/Main.java
@@ -10,14 +10,16 @@ public class Main {
 
 		double intervalo = sc.nextDouble();
 
-		if (intervalo <= 25) {
+		if (intervalo >= 0 && intervalo <= 25) {
 			System.out.println("Intervalo [0,25]");
-		} else if (intervalo <= 50) {
+		} else if (intervalo > 25 && intervalo <= 50) {
 			System.out.println("Intervalo [25,50]");
-		} else if (intervalo <= 75) {
+		} else if (intervalo > 50 && intervalo <= 75) {
 			System.out.println("Intervalo [50,75]");
-		} else if (intervalo <= 100) {
+		} else if (intervalo > 75 && intervalo <= 100) {
 			System.out.println("Intervalo [75,100]");
+		} else if (intervalo < 0 || intervalo > 100) {
+			System.out.println("Fora do intervalo");
 		}
 		sc.close();
 
