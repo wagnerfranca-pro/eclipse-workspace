d3cfda7338f2b9fa0b725bc575a0837ebd37d0da
wagnerfranca-pro <wagnerdesouzafranca@gmail.com>
2026-03-03 05:55:51 -0300

    Finalizado o exercicio 'imposto-renda'.

--- imposto-renda/src/Main.java
+++ imposto-renda/src/Main.java
@@ -10,22 +10,26 @@ public class Main {
 		Scanner sc = new Scanner(System.in);
 
 		double salario = sc.nextDouble();
-		double imposto01 = 0;
-		double imposto02 = 0;
-		double imposto03 = 0;
+		double valor01 = 0.00;
+		double valor02 = 0.00;
+		double valor03 = 0.00;
+		double valor04 = 0.00;
 
 		if (salario > 0.00 && salario <= 2000.00) {
 			System.out.println("Isento");
-		} else if (salario > 2000.01 && salario <= 3000.00) {
-			imposto01 = ((salario) - (3000.00)) * 0.08;
-			System.out.printf("R$ %.2f", imposto01);
-		} else if (salario > 3000.01 && salario <= 4500.00) {
-			imposto01 = 1000.00 * 0.08;
-			imposto02 = (4500.00-salario) * 0.18;
-			System.out.printf("R$ %.2f", imposto01 +  imposto02);
-		} else if (salario > 4500.00) {
-			imposto03 = ((4500.00) - (salario)) * 0.28;
-			System.out.printf("R$ %.2f", imposto03);
+
+		} else if (salario > 2000.01 && salario <= 4500.00) {
+			valor01 = salario - 2000.00; // 3002 - 2000 = 1002
+			valor02 = valor01 - 1000.00; // 1002 - 1000 = 2 
+			valor03 = valor02 * 0.18; // 2 * 0.18 = 0.36
+			valor04 = (salario - 2000.00 - valor02) * 0.08; // 3002 - 2000 - 0.36 = 80
+			System.out.printf("R$ %.2f", valor03 + valor04);
+
+		} else {
+			valor01 = 1000.00 * 0.08; // 80
+			valor02 = 1500.00 * 0.18; // 270
+			valor03 = (salario - 4500) * 0.28; // 4520 - 4500 = 5.6
+			System.out.printf("R$ %.2f", valor01 + valor02 + valor03);
 		}
 		sc.close();
 	}
