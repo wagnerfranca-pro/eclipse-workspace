21b61b341317330baa1c7cf2fae17702410572b8
Wagner de Souza França <wagnerdesouzafranca@gmail.com>
2026-04-15 21:07:28 -0300

    Changed ok

--- course01/src/application/Program.java
+++ course01/src/application/Program.java
@@ -12,22 +12,27 @@ public class Program {
 
 		Scanner sc = new Scanner(System.in);
 
-		
+		Product p = new Product();
+				
 		System.out.println("Enter product data:");
 		System.out.print("Name: ");
 		String name = sc.nextLine();
 		System.out.print("Price: ");
 		double price = sc.nextDouble();
-		System.out.print("Quantity in stock: ");
-		int quantity = sc.nextInt();
-		Product product = new Product(name, price, quantity);
 
+		Product product = new Product(name, price);
+
+		product.setName("Computer");
+		System.out.println("Updated name: " + product.getName());
+		product.setPrice(1200);
+		System.out.println("Updated price: " + product.getPrice());
+		
 		System.out.println();
 		System.out.println("Product data: " + product);
 
 		System.out.println();
 		System.out.println("Enter the number of products to be added in stock: ");
-		quantity = sc.nextInt();
+		int quantity = sc.nextInt();
 		product.addProducts(quantity);
 
 		System.out.println();
